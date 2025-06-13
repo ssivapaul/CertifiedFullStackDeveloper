d63703751f818f1dabe4581d65a0b43a024e6acd
Siva Sivapalan <ssivapaul@gmail.com>
2025-06-13 23:55:08 +1000

    Build A Sorting Visualiser, second commit..

--- BuildingASortingVisualiser/script.js
+++ BuildingASortingVisualiser/script.js
@@ -41,8 +41,36 @@ let highlightCurrentEls = (el, i) => {
   el.children[i + 1].style.border = "1px dashed red";
 };
 
-let btn = document.getElementById("generate-btn");
-btn.addEventListener("click", () => {
-  let startingArray = document.getElementById("starting-array");
+let btnGenerate = document.getElementById("generate-btn");
+btnGenerate.addEventListener("click", () => {
+  const startingArray = document.getElementById("starting-array");
+  const arrayContainer = document.getElementById("array-container");
+  const children = arrayContainer.children;
+  for (let i = children.length - 1; i >= 0; i--) {
+    if (children[i] !== startingArray) {
+      arrayContainer.removeChild(children[i]);
+    }
+  }
   fillArrContainer(startingArray, generateArray());
+});
+
+let btnSort = document.getElementById("sort-btn");
+btnSort.addEventListener("click", () => {
+  let arrayContainer = document.getElementById("array-container");
+  let spanArray = [];
+  let originalSpanContents = [];
+  let newSpanContents = [];
+  let spanContents = [];
+  let children = arrayContainer.children;
+  let originalSpanArray = Array.from(children[0].children);
+  originalSpanContents = originalSpanArray.map((child) =>
+    Number(child.textContent)
+  );
+
+  for (let i = 0; i < 4; i++) {
+    newSpanContents = swapElements(originalSpanContents, i); // Swap Elements, if need
+    let newChildArray = generateContainer();
+    arrayContainer.appendChild(newChildArray);
+    fillArrContainer(newChildArray, newSpanContents); // Fill the swapped array
+  }
 });
---
+++ BuildingASortingVisualiser/test1.js
@@ -0,0 +1,13 @@
+let array1 = [1, 2, 3];
+let array2 = [4, 5, 6];
+
+//array1.length = 0;         // Clear array1
+//array1.push(...array2);    // Push all elements from array2
+
+//array1 = [...array2];
+//array1 = array2.slice();
+
+array1 = array2
+
+console.log(array1)
+console.log(array2)
